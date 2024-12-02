c3e199c1b7afd26e402b0d41d0219fa57a8307e2
Usmanbello00 <108652858+Usmanbello00@users.noreply.github.com>
2024-12-03 00:27:58 +0100

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -1,8 +1,8 @@
 <?php
 
-$host = getenv("DB_HOST") ?: "localhost";
-$user = getenv("DB_USER") ?: "root";
-$password = getenv("DB_PASSWORD") ?: "vijai@123";
+$host = getenv("DB_HOST") ?: "nnpcserver.mysql.database.azure.com";
+$user = getenv("DB_USER") ?: "nnpc";
+$password = getenv("DB_PASSWORD") ?: "Password@123";
 $database = getenv("DB_DATABASE") ?: "scoe";
 
 $conn = new mysqli($host, $user, $password, $database);
